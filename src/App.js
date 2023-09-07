2c8d774f727e3a18c7f0a4337d85f5da0cc66de3
phantomKid13 <chiragnawale1234@gmail.com>
2023-09-07 18:17:05 +0530

    added react-router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,56 @@
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import FormControl from "./components/FormControl";
-import React, {useState} from "react";
-import Alert from "./components/Alert";
 import About from "./components/About";
+import Alert from "./components/Alert";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
 
-  const [mode,setMode] = useState("light")
+  const [mode, setMode] = useState("light")
   const [alert, setAlert] = useState(null)
 
   const setAlertFn = (msg, type) => {
     setAlert({
-      msg : msg,
-      type : type
+      msg: msg,
+      type: type
     })
-    
+
     setTimeout(() => {
       setAlert(null)
     }, 2000);
   }
 
-  const toggleMode = () =>{
-    if(mode==="light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#212529"
-      setAlertFn("Switched to Dark Mode","success")
+      setAlertFn("Switched to Dark Mode", "success")
     }
-    else{
+    else {
       setMode("light")
       document.body.style.backgroundColor = "white"
-      setAlertFn("Switched to Light Mode","success")
+      setAlertFn("Switched to Light Mode", "success")
     }
     console.log(mode)
   }
 
   return (
-    <div className="App">
-      <Navbar toggle={toggleMode}></Navbar>
-      <Alert alert={alert} setAlert={setAlert}></Alert>
-      <FormControl mode={mode} setAlertFn={setAlertFn}></FormControl>
-      <About></About>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar toggle={toggleMode}></Navbar>
+        <Alert alert={alert} setAlert={setAlert}></Alert>
+        <Routes>
+          <Route exact path="/" element={<FormControl mode={mode} setAlertFn={setAlertFn}></FormControl>} />
+          <Route exact path="/about" element={<About></About>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
